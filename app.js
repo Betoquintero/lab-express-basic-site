471729a19ca56cd51c30076ff665269518470771
Alberto <aquintero88@gmail.com>
2022-05-29 15:00:32 +0200

    lab done

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require ('express')
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (request, res, next) => {
+    res.sendFile(__dirname + '/views/home.html')    
+})
+
+app.get("/about", (request, res,next) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get("/gallery", (request, res, next) => {
+    res.sendFile(__dirname + '/views/gallery.html')
+})
+
+app.listen(3000, () => console.log('Connected on port 3000!'))
